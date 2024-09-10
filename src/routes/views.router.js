59a20e2b0dfe513317c8eed4bc6d99cabe1443c0
Martin Hernandez <132721254+Tincho83@users.noreply.github.com>
2024-09-10 00:41:51 -0300

    v3i

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -53,31 +53,106 @@ router.get('/productsdb', async (req, res) => {
 router.get('/realtimeproductsdb', async (req, res) => {
     console.log("\r\nentro get");
 
+    let titulo = "Listado de Productos en tiempo Real";
+    let prodss;
+    let dataObjectPaginate = {};
+    let dataObject = {};
+    let cSort = {};
+
     let { detalle } = req.query;
     if (detalle) {
         console.log(detalle);
     }
 
-    let { page, limit, sort, query } = req.query;
+    let { page, limit, sort, query, type } = req.query;
+
     if (!page || isNaN(Number(page))) {
         page = 1;
     }
+    console.log(`pagina: ${page}`);
 
-    let titulo = "Listado de Productos en tiempo Real";
-    let prodss;
-    let dataObjectPaginate = {};
 
-    sort = { category: -1 };
+    if (!limit || isNaN(Number(limit))) {
+        limit = 10;
+    }
+    console.log(`limite: ${limit}`);
+
+    if (!sort) {
+        console.log(`orden no definida: ${sort}`);
+    } else {
+        let criteriosSep = sort.split(',');
+
+        criteriosSep.forEach(element => {
+            let [criterio, orden] = element.split(':');
+
+            console.log(`Criterio: ${criterio}, Orden: ${orden}`);
+
+            let valorOrden = (orden === 'asc') ? 1 : (orden === 'desc') ? -1 : null;
+
+            if (valorOrden !== null) {
+                // Agregar el criterio y el valor al objeto cSort
+                cSort[criterio] = valorOrden;
+            }
+
+        });
+
+        console.log(`Criterios de orden definidos:`, cSort);
+    }
+
+    let searchCriteria = {};
+
+
+
+
+
+
+
+
+    //http://localhost:8080/realtimeproductsdb?page=1&sort=category:desc,price:desc&limit=7
+
+    //sort = { category: -1, price: 1 };
+    //sort = { price: 1 };
 
     try {
-        console.log(`Paginas ${page}`);
-        //prodss = await ProductsManagerMongoDB.getProductsDBMongo();
-        prodss = await ProductsManagerMongoDB.getProductsDBMongoPaginate(page, limit, sort);
-        console.log(`Products: ${JSON.stringify(prodss.docs, null, 5)}`);
-        console.log(`Se eencontraron ${prodss.docs.length} productos.`);
-        console.log("::", prodss);
 
+        if (!query) {
+            console.log('Realizando búsqueda general');
 
+            //console.log(`Paginas ${page}`);
+            //prodss = await ProductsManagerMongoDB.getProductsDBMongo();
+            prodss = await ProductsManagerMongoDB.getProductsDBMongoPaginate(page, limit, cSort, query);
+            //console.log(`Products: ${JSON.stringify(prodss.docs, null, 5)}`);
+            //console.log(`Se eencontraron ${prodss.docs.length} productos.`);
+            //console.log("::", prodss);
+
+        } else {
+            console.log(`query: ${query}`);
+            console.log(`type: ${type}`);
+            // Construir el criterio de búsqueda con base en el tipo de filtro
+            if (type === 'category') {
+                searchCriteria = { category: query };
+            } else if (type === 'price') {
+                searchCriteria = { price: query };
+            } else if (type === 'title') {
+                searchCriteria = { title: new RegExp(query, 'i') }; // Búsqueda insensible a mayúsculas/minúsculas
+            }
+
+            // Aquí utilizamos el criterio de búsqueda en la consulta
+            prodss = await ProductsManagerMongoDB.getProductsByDBMongo(searchCriteria);
+        }
+
+        dataObject = {
+            status: 'success',
+            payload: prodss.docs,
+            totalPages: prodss.totalPages,
+            prevPage: prodss.prevPage,
+            nextPage: prodss.nextPage,
+            page: prodss.page,
+            hasPrevPage: prodss.hasPrevPage,
+            hasNextPage: prodss.hasNextPage,
+            prevLink: prodss.hasPrevPage ? `/realtimeproductsdb?page=${prodss.prevPage}&limit=${limit}` : null,
+            nextLink: prodss.hasNextPage ? `/realtimeproductsdb?page=${prodss.nextPage}&limit=${limit}` : null
+        };
 
         dataObjectPaginate = {
             //titulo,
@@ -90,7 +165,7 @@ router.get('/realtimeproductsdb', async (req, res) => {
             nextPage: prodss.nextPage,        // Siguiente página
         };
 
-        console.log("Datos para la vista:", dataObjectPaginate);
+        //console.log("Datos para la vista:", dataObjectPaginate);
 
     } catch (error) {
         console.log(error);
